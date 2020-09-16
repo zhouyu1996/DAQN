4750004acf4055121c096762173e03db825e140e
zhouyu <18990848997@163.com>
2020-09-16 20:41:43 +0800

    config file

--- config.py
+++ config.py
@@ -15,7 +15,6 @@ device = 0
 # code length = subspace_num * subspace_num * log2(subcenter_num) = 8*subspace_num
 subspace_num = 4
 
-# please modify these when change datasets
 txt_dim = 1386
 label_dim = 24
 image_width = 256
@@ -25,8 +24,10 @@ image_channel = 3
 subcenter_num = 256
 output_dim = 128
 
-# triplet loss margin
+# setting for our proposed boundary controlled triplet loss
 margin = 0.5
+margin_p = 0.7
+margin_n = 0.3
 # weights of loss function
 beta = [0.3, 1.5, 0.01]
 
@@ -34,7 +35,6 @@ lr_vgg = 0.001
 lr_dis = 0.001
 lr_label = 0.001
 
-# modify these two parameters following README file to get a better results for different datasets
 lr_img = 0.06
 lr_txt = 0.03
 
@@ -50,5 +50,6 @@ batch_size = 64
 val_batch_size = 100
 code_batch_size = 100
 training_epoch = 30
+
 # map@50
 R = 50
